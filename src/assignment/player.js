8f914e93c245089ab7823018d806b94e0416455c
Arjun G <arjupba@gmail.com>
2022-12-30 00:58:07 +0530

    Promise.all implemented

--- src/assignment/player.js
+++ src/assignment/player.js
@@ -1,88 +1,61 @@
 /*
  * Feel free to rename this file `player.ts` and remove `player.d.ts` if you are more comfortable with TypeScript.
  */
-import workerJs from './worker.js';
+// import workerJs from './worker.js';
 import {delayAsync} from '../util';
 
-const loadWebWorker = (worker) => {
-  const code = worker.toString();
-  const blob = new Blob(['(' + code + ')()']);
+// const loadWebWorker = (worker) => {
+//   const code = worker.toString();
+//   const blob = new Blob(['(' + code + ')()']);
 
-  return new Worker(URL.createObjectURL(blob));
-};
+//   return new Worker(URL.createObjectURL(blob));
+// };
 
-const completePlay = ({worker}) => {
-  return new Promise((resolve) => {
-    worker.onmessage = (event) => {
-      resolve(undefined);
-    };
-  });
-};
+// const completePlay = ({worker}) => {
+//   return new Promise((resolve) => {
+//     worker.onmessage = (event) => {
+//       resolve(undefined);
+//     };
+//   });
+// };
 
-async function playTrack(synthesizer, track) {
-  const worker = loadWebWorker(workerJs);
-  const channel = synthesizer.getChannel(track.instrumentName);
+function playTrack(synthesizer, track) {
+  const playTrackPromise = () =>
+    new Promise(async (resolve, reject) => {
+      const channel = synthesizer.getChannel(track.instrumentName);
 
-  for (const note of track.notes) {
-    const isPlaying = channel.playNote(note.name, 100);
-    console.log(1, isPlaying);
-    if (!isPlaying) {
-      break;
-    }
-    console.log(2);
-    worker.postMessage({duration: note.duration});
-    console.log(3);
-    await completePlay({worker});
-    console.log(4);
+      for (const note of track.notes) {
+        const isPlaying = channel.playNote(note.name, 100);
 
-    channel.stopNote();
-  }
+        if (!isPlaying) {
+          break;
+        }
+        // worker.postMessage({duration: note.duration});
+        // await completePlay({worker});
+
+        await delayAsync(note.duration);
+
+        channel.stopNote();
+      }
+
+      resolve('track play completed');
+    });
+
+  return playTrackPromise;
+  // const worker = loadWebWorker(workerJs);
 }
 
 export async function player(synthesizer, tracks) {
-  console.log(synthesizer, tracks);
-  // Write your code here.
-  // throw new Error('You need to implement the player!');
-  const track = tracks[0];
-  // tracks.forEach(async (track) => {
-  //   // const track = tracks[index];
+  return new Promise((resolve) => {
+    const trackPromiseArray = [];
 
-  //
-  //   const channel = synthesizer.getChannel(track.instrumentName);
+    tracks.forEach((track) => {
+      trackPromiseArray.push(playTrack(synthesizer, track));
+    });
 
-  //   for (const note of track.notes) {
-  //     const isPlaying = channel.playNote(note.name, 100);
-
-  //     if (!isPlaying) {
-  //       break;
-  //     }
-
-  //     await delayAsync(note.duration);
-
-  //     channel.stopNote();
-  //   }
-  // });
-
-  // tracks.forEach((track) => {
-  // const channel = synthesizer.getChannel(track.instrumentName);
-  // playTrack(synthesizer, tracks[0]);
-  // });
-
-  const worker = loadWebWorker(workerJs);
-  const channel = synthesizer.getChannel(track.instrumentName);
-
-  for (const note of track.notes) {
-    const isPlaying = channel.playNote(note.name, 100);
-    console.log(1, isPlaying);
-    if (!isPlaying) {
-      break;
-    }
-    console.log(2);
-    worker.postMessage({duration: note.duration});
-    console.log(3);
-    await completePlay({worker});
-    console.log(4);
-
-    channel.stopNote();
-  }
+    Promise.all(trackPromiseArray.map((item) => item())).then((values, err) => {
+      console.log(values, err);
+      resolve('all track play completed');
+    });
+  });
 }
