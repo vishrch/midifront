bbf838eeb5fabaca6b202aef7fca5a4bf67e6b5a
Arjun G <arjupba@gmail.com>
2022-12-30 02:29:43 +0530

    buildTimer function integrated

--- src/assignment/player.js
+++ src/assignment/player.js
@@ -1,18 +1,21 @@
-import {delayAsync} from '../util';
+import {delayAsync, buildTimer} from '../util';
 
-const playTrack = (synthesizer, track) => () =>
+const playTrack = (synthesizer, track, timer) => () =>
   new Promise(async (resolve, reject) => {
     const channel = synthesizer.getChannel(track.instrumentName);
+    let lastPlayedTime = 0;
 
     for (const note of track.notes) {
       try {
-        const isPlaying = channel.playNote(note.name, 100);
+        const isPlaying = channel.playNote(note.name, note.velocity);
 
         if (!isPlaying) {
           break;
         }
 
-        await delayAsync(note.duration);
+        await delayAsync(note.duration + lastPlayedTime - timer());
+
+        lastPlayedTime = lastPlayedTime + note.duration;
 
         channel.stopNote();
       } catch (error) {
@@ -26,10 +29,9 @@ const playTrack = (synthesizer, track) => () =>
 export const player = async (synthesizer, tracks) =>
   new Promise((resolve, reject) => {
     const trackPromiseArray = [];
+    const timer = buildTimer();
 
-    tracks.forEach((track) => {
-      trackPromiseArray.push(playTrack(synthesizer, track));
-    });
+    tracks.forEach((track) => trackPromiseArray.push(playTrack(synthesizer, track, timer)));
 
     Promise.all(trackPromiseArray.map((item) => item()))
       .then(() => resolve('all track play completed'))
